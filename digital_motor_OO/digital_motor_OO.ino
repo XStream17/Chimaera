ed8bc46bb28be5432cb82d77152fe14d672d2489
junbin07 <30484884+junbin07@users.noreply.github.com>
2017-10-26 21:37:16 +0800

    Add files via upload

--- digital_motor_OO/digital_motor_OO.ino
+++ digital_motor_OO/digital_motor_OO.ino
@@ -1,19 +1,19 @@
 #include "uart.h"
 #include "motor.h"
 
+uart bluetooth(96,'z');
+motor m1(5,6,10,9);
 
-uart bluetooth (96,'z');
-motor *dir (5,6,10,9);
 void setup() {
   // put your setup code here, to run once:
    bluetooth.initialize();
-   dir->motor_init(5,6,10,9);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  char char_data = bluetooth.receive_char();
-  dir->control(char_data);
+  char char_data = 0;
+  char_data = bluetooth.receive_char();
+  m1.control(char_data);
   
 }
--- main/main.ino
+++ main/main.ino
@@ -1,8 +1,10 @@
 #include "uart.h"
 #include "sg90.h"
+#include "motor.h"
 
 uart bluetooth (96,'z');
 claw claw1(11);
+motor m1(5,6,10,9);
  
 int angle = 0;   // servo position in degrees 
  
@@ -15,8 +17,11 @@ void setup()
  
 void loop() 
 { 
-  char char_data = bluetooth.receive_char();
+  char char_data = 0;
+  char_data = bluetooth.receive_char();
+  m1.control(char_data);
   if (char_data != 0)
     Serial.write(char_data);
   claw1.whentograb(char_data);
 } 
+
