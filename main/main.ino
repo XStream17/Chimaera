44b84caefbcec967bfb95bc7d11ff65b63e8a28e
LeoYong95 <y_c_l_@live.com>
2017-10-31 17:24:08 +0800

    remove test file

--- main/main.ino
+++ main/main.ino
@@ -5,24 +5,27 @@
 #include "sg90.h"
 #include "l293d.h"
 
-bluetooth bluetooth (96,'z');
-claw claw1(11);
-l293d m1(5,6,10,9);
+bluetooth *b1;
+claw *claw1;
+l293d *m1;
  
 int angle = 0;   // servo position in degrees 
  
 void setup() 
 { 
+  b1 = new bluetooth(96,'z');
+  claw1 = new claw(11);
+  m1 = new l293d(5,6,10,9);
 }
  
  
 void loop() 
 { 
   char char_data = 0;
-  char_data = bluetooth.receive_char();
-  m1.control(char_data);
+  char_data = b1->receive_char();
+  m1->control(char_data);
   if (char_data != 0)
     Serial.write(char_data);
-  claw1.whentograb(char_data);
+  claw1->whentograb(char_data);
 } 
 
